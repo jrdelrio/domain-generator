1b26025e37370bf1a771f80b255c00f7bdab2c7d
Jose Raimundo Del Rio <jrdelriodom@gmail.com>
2022-11-18 18:53:20 +0000

    Upload domain generator script with different extensions

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "my", "your", "mydgos"];
+  let adjetive = ["great", "big", "fat", "small", "misterious", "black"];
+  let noun = ["jogger", "racoon", "food", "nap", "house", "computer", "music"];
+  let domains = [".com", ".cl", ".ve", ".it", ".es", ".ag"];
+
+  let combinations = 0;
+
+  for (let art of pronoun) {
+    for (let adj of adjetive) {
+      for (let sust of noun) {
+        for (let dom of domains) {
+          let domain = `${art}${adj}${sust}${dom}`;
+          console.log(domain);
+          combinations++;
+        }
+      }
+    }
+  }
+  console.log(combinations);
 };
